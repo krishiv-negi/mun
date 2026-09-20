0ccd107d62af0059b461cf0276ea5c88939289ea
Ayush Badoni <ayushbadoni3840@gmail.com>
2026-09-21 00:01:07 +0530

    Add search and update features

--- backend/Delegate.cpp
+++ backend/Delegate.cpp
@@ -3,9 +3,12 @@
 using namespace std;
 class Delegate{
     string name, country;
-    int age;
+    int age, num, mun_attend, num1;
+    vector<string> prev_committees;
+    vector<string>awards;
     public:
         void input(){
+            string committee;
             cout<<"Enter Delegate Name: ";
             getline(cin,name);
             cout<<"Enter Age: ";
@@ -13,21 +16,135 @@ class Delegate{
             cin.ignore();
             cout<<"Enter Country: ";
             getline(cin,country);
+        
+            cout<<"How many previous committees attends? : ";
+            cin>>num;
+            cin.ignore();
+            cout<<"Enter Previous Committees name: ";
+            for(int i=0; i<num; i++){
+            getline(cin,committee);
+            prev_committees.push_back(committee);
+        }
+        cout<<"How many MUN have you attended?: ";
+        cin>>mun_attend;
+        cout<<"How many awards have you received?: ";
+        cin>>num1;
+        cin.ignore();
+        for(int i=0;i<num1; i++){
+            string award;
+            cout<<"Enter award: "<<i+1<<":";
+            getline(cin,award);
+            awards.push_back(award);
+        }
         }
         void display(){
+            int i=0;
             cout<<"Name: "<<name<<endl;
             cout<<"Age: "<<age<<endl;
             cout<<"Country: "<<country<<endl;
+            cout<<"Previous Committee: "<<endl;
+            for(string c: prev_committees){
+                cout<<i+1<<". ";
+            cout<< c <<" "<<endl;
+            i++;
+            }
+            cout<<"MUN Attended: "<<mun_attend;
             cout<<endl<<endl;
-        }
-};
+            cout<<"Awards: "<<endl;
+            i=0;
+            for(string award:awards){
+                cout<<i+1<<". "<<award<<" "<<endl;
+            }
+            cout<<endl;
+            }
+        
+        void search();
+        void update();
+        };
+
 vector<Delegate> delegates;
 
+void Delegate:: search(){
+            string s;
+            cout<<"\nEnter Delegate Name: ";
+            getline(cin,s);
+            for(Delegate d: delegates){
+                if(d.name==s){
+                    cout<<"\nName Found!\n"<<endl;
+                     int i=0;
+                cout<<"Name: "<<d.name<<endl;
+                cout<<"Age: "<<d.age<<endl;
+                cout<<"Country: "<<d.country<<endl;
+                cout<<"Previous Committee: "<<endl;
+                for(string c: d.prev_committees){
+                cout<<i+1<<". ";
+                 cout<< c <<" "<<endl;
+                i++;
+                 }
+                cout<<"MUN Attended: "<<d.mun_attend;
+                cout<<endl<<endl;
+                cout<<"Awards: "<<endl;
+                i=0;
+                for(string award:d.awards){
+                cout<<i+1<<". "<<award<<" "<<endl;
+                }
+                 
+                    
+                }
+            }
+
+}
+void Delegate:: update(){
+     string s;
+            cout<<"\nEnter Delegate Name: ";
+            getline(cin,s);
+            for(Delegate d: delegates){
+                if(d.name==s){
+                    int ch,n;
+                    cout<<"\nName Found!\n"<<endl;
+                    cout<<"What do you want to update?"<<endl<<"1.Age"<<endl
+                        <<"2.Country"<<endl<<"3.Previous Committees"<<endl
+                        <<"4.MUN Attended"<<endl<<"5.Awards"<<endl<<"Enter choice: ";
+                    switch(ch){
+                        case 1:
+                            cout<<"Enter new Age: ";
+                            cin>>d.age;
+                            break;
+                        case 2:
+                            cout<<"Enter new Country: ";
+                            cin>>d.country;
+                            break;
+                        case 3:
+                            cout<<"How many Previous committees?: ";
+                            cin>>n;
+                            d.prev_committees.resize(n);
+                            for(int i=0; i<n; i++){
+                                cout<<"Enter Previous Committee "<<i+1<<": ";
+                                cin>>d.prev_committees[i];
+                            }
+                            break;
+                        case 4:
+                            cout<<"Enter new MUN Attended: ";   
+                            cin>>d.mun_attend;
+                            break;
+                        case 5:
+                            cout<<"How many Awards?: ";
+                            cin>>n;
+                            d.awards.resize(n);
+                            for(int i=0; i<n; i++){
+                                cout<<"Enter Award "<<i+1<<": ";
+                                cin>>d.awards[i];
+                            }
+                            break; 
+                     }
+                 }
+            }
+}
 int main(){
     int ch;
     do{
         cout<<"----Delegate Management----"<<endl;
-        cout<<"1. Add Delegate\n2.Display Delegates\n3.Exit\n Enter the choice: ";
+        cout<<"1. Add Delegate\n2.Display Delegate\n3.Search Delegate\n 4.Update Delegate\n5.Exit\nEnter the choice: ";
         cin>>ch;
         cin.ignore();
         Delegate d;  
@@ -42,9 +159,15 @@ int main(){
                 d.display();
                 }
                 break;
+            case 3:
+                d.search();
+                break;
+            case 4:
+                d.update();
+                break;
             
         }
     }
-    while(ch<=2);
+    while(ch<=4);
     return 0;
 }
